76af4ff376be89ad7f95a06b2a1e57ac028954ff
Damion Sheppard <damionlsheppard@gmail.com>
2020-04-24 15:30:20 +0100

    Added an Input testing Scene, and a simpleController script that moves a square and changes it's colour when you press 'Attack'. Attack currently has 3 bindings - Left Mouse, Space Bar, and Game Pad 'West' (Xbox X, etc). Movement works with WASD and Left Joystick

---
+++ Assets/Scripts/SimpleController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.U2D;
+
+public class SimpleController : MonoBehaviour
+{
+    private PlayerInputActions _inputActions;
+
+    private Transform _transform;
+    private Vector2 _direction;
+
+    private void Awake()
+    {
+        _inputActions = new PlayerInputActions();
+
+        _transform = this.transform;
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Game.Enable();
+
+        _inputActions.Game.Move.performed += OnMove;
+        _inputActions.Game.Move.canceled += OnMove; // We need an event for when you STOP sending a direction, otherwise it won't get reset to (0,0) (eg. letting go of stick)
+        _inputActions.Game.Attack.performed += OnAttack;
+    }
+
+    private void Update()
+    {
+        float frameRateScaler = 10 * Time.deltaTime;
+        _transform.position += new Vector3(_direction.x, _direction.y, 0) * frameRateScaler;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _direction = context.ReadValue<Vector2>();
+    }
+
+    public void OnAttack(InputAction.CallbackContext _)
+    {
+        // Setting a random color yay
+        Material mat = GetComponent<SpriteShapeRenderer>().material;
+        if(mat != null) mat.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Game.Attack.performed -= OnAttack;
+        _inputActions.Game.Move.canceled -= OnMove;
+        _inputActions.Game.Move.performed -= OnMove;
+
+        _inputActions.Game.Disable();
+    }
+}
