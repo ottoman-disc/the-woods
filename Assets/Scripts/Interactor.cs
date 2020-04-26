b5db2677d704e04108b0c495eb90bafac599560a
Damion Sheppard <damionlsheppard@gmail.com>
2020-04-26 12:08:37 +0100

    Testing addForce

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -4,6 +4,8 @@ public class Interactor : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.collider.GetComponent<IInteractable>().Interact();
+        collision.collider.GetComponent<IInteractable>()?.Interact();
+
+        this.GetComponent<Rigidbody2D>().AddForce(Vector2.left * 1f, ForceMode2D.Impulse);
     }
 }
