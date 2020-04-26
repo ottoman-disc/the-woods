2ec2e89054cc82197c84c4ee003d85112fd7e72e
Scott Matthews <scottmatthews343@gmail.com>
2020-04-26 18:11:29 +0100

    implementing simple lobby and network movement

---
+++ Assets/Scripts/GameLauncher.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace OttomanDisk
+{
+    // Handles launching a new game from the title screen using PUN
+    // PUN library provides MonoBehaviourCallbacks class to simplyfy implementing callbacks
+    public class GameLauncher : MonoBehaviourPunCallbacks
+    {
+        // Private fields
+        string gameVersion = "1"; // increment for breaking changes to networking
+        byte maxPlayers = 4; // define maximum allowed players in the room
+        bool userRequestedConnection; // determine if the user requested a connection
+
+        [SerializeField]
+        private GameObject menuPanel;
+
+        [SerializeField]
+        private GameObject statusText;
+
+        // MonoBehaviour Callbacks
+        void Awake()
+        {
+            // this allows the master client to load levels for all players
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+
+        void Start()
+        {
+            menuPanel.SetActive(true);
+            statusText.SetActive(false);
+        }
+
+        // MonoBehaviousPunCallbacks
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("OnConnectedToMaster() called");
+            // when returning to the title screen this callback is triggered again
+            // only join a game is it was a user request
+            if (userRequestedConnection)
+            {
+                Join();
+            }
+        }
+
+        public override void OnDisconnected(Photon.Realtime.DisconnectCause cause)
+        {
+            Debug.LogWarningFormat("Disconnected from PUN with reason '{0}'", cause);
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            PhotonNetwork.CreateRoom(null, new RoomOptions
+            {
+                MaxPlayers = maxPlayers
+            });
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("Joined room");
+            PhotonNetwork.LoadLevel(1);
+        }
+
+        // Other Methods
+        public void Connect()
+        {
+            menuPanel.SetActive(false);
+            statusText.SetActive(true);
+
+            if (PhotonNetwork.IsConnected)
+            {
+                Join();
+            }
+            else
+            {
+                userRequestedConnection = PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = gameVersion;
+                // calback will handle Join() in this case
+            }
+        }
+
+        private void Join()
+        {
+            // placeholder for better join logic
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace OttomanDisk
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        public GameObject playerPrefab; 
+
+        // Monobehaviour Methods
+        void Start()
+        {
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'",this);
+            }
+            else 
+            {
+                Debug.Log("Instantiating local player");
+                PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(1f,2f,0f), Quaternion.identity, 0);
+            }
+        }
+
+        void Update()
+        {
+
+        }
+
+        // MonobehaviourPunCallbacks Methods
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        // public override void OnPlayerEnteredRoom(Player newPlayer)
+        // {
+        //     Debug.Log("A new player has entered room");
+
+        //     if (PhotonNetwork.IsMasterClient)
+        //     {
+        //         // only the master has rights to load a new scene
+        //         LoadArea();
+        //     }
+        // }
+
+        // Other Methods
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        // private void LoadArea()
+        // {
+        //     if (!PhotonNetwork.IsMasterClient)
+        //     {
+        //         Debug.LogError("non master trying to load level??");
+        //     }
+        //     PhotonNetwork.LoadLevel(1);
+        // }
+
+    }
+
+}
