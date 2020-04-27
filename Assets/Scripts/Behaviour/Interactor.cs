c2f7a128723f68fbb7c4d87b5d7c852fb2210151
DamionSheppard <d.l.sheppard@live.co.uk>
2020-04-27 19:32:39 +0100

    Interactables and Collisions (#6)
    * started looking at rotation for the player character
    
    * added in IInteract interface
    
    * added a simple test 'tree' script
    
    * Rotation was a dumb distraction. Not relevant. Removed for now.
    
    * Turned Physics 2D Gravity off. We are top down for now, so we don't want this.
    
    * Tree will change colour when it is interacted with.
    
    * Refactored the SimpleController into a PlayerController and Motor. SimpleController depracated. Setup physics based movement - in other words, we are no longer setting the transform directly - instead we set the velocity of the newly attached rigidbody2d. This is really unrelated to Interactables, but this allows easy collisions, which we can use as placeholder interactions.
    
    * Player can now 'interact' with IInteractables simply by bumping into them.
    
    * Using rb.MovePosition instead of setting velocity directly.
    
    * Testing addForce
    
    * PlayerController requires Motor component
    
    * Reorganised Behaviour scripts into a behaviour folder
    
    * Renamed objects in scene - both players (old and physics) aptly named
    
    * Removed obsolete AddForce. More importantly, removed incorrect null propogation (not actually correct for MonoBehaviours)
    
    * Motor now has separation between Move and MoveStop. This is so we can turn 'drag' off when the player is moving around
    
    * Moved Tree script into Behaviours folder to appease my soul
    
    * Added a test explosion to see if the rigidbodies are reacting the way we want. They aren't! But we'll tame that later.
    
    Co-authored-by: Damion Sheppard <damionlsheppard@gmail.com>

---
+++ Assets/Scripts/Behaviour/Interactor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        IInteractable interactable = collision.collider.GetComponent<IInteractable>();
+        if (interactable != null) interactable.Interact();
+    }
+}
---
+++ Assets/Scripts/Behaviour/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Motor))]
+public class PlayerController : MonoBehaviour
+{
+    private PlayerInputActions inputActions;
+
+    private Motor moter;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+
+        moter = GetComponent<Motor>();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Game.Enable();
+
+        inputActions.Game.Move.performed += OnMove;
+        inputActions.Game.Move.canceled += OnMoveStop;
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        moter.Move(context.ReadValue<Vector2>());
+    }
+
+    private void OnMoveStop(InputAction.CallbackContext context)
+    {
+        moter.Stop();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Game.Move.canceled -= OnMoveStop;
+        inputActions.Game.Move.performed -= OnMove;
+
+        inputActions.Game.Disable();
+    }
+}
