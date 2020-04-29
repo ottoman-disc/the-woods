7592cafb263f1bda91bd152db1e9053c9a49f6be
Damion Sheppard <damionlsheppard@gmail.com>
2020-04-29 15:30:14 +0100

    Added a handy Scene Loader that adds a Menu (at the very top of Unity) for easy loading of common scenes.

---
+++ Assets/Scripts/Debug/MenuSceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class MenuSceneLoader
+{
+    private const string LobbyScene = "Assets/Scenes/TitleScene.unity";
+    private const string GameScene = "Assets/Scenes/Testing/Input-ds.unity";
+
+    [MenuItem("Scene/Lobby")] 
+    private static void Lobby() => OpenScene(LobbyScene);
+
+    [MenuItem("Scene/Game")] 
+    private static void Game() => OpenScene(GameScene);
+
+    private static void OpenScene(string scene) => EditorSceneManager.OpenScene(scene);
+}
