29a5729b807fd52894a0e6d31792baad12a3b7e5
Damion Sheppard <damionlsheppard@gmail.com>
2020-04-29 12:37:22 +0100

    Added explanation comments to PhotonPlayerView

--- Assets/Scripts/Behaviour/Network/PhotonPlayerView.cs
+++ Assets/Scripts/Behaviour/Network/PhotonPlayerView.cs
@@ -9,12 +9,19 @@ public class PhotonPlayerView : MonoBehaviourPun
 
         if (!photonView.IsMine)
         {
+            // Remote player does not need a Controller or Motor. Movement driven
+            // by them locally, and synced to us.
             Destroy(GetComponent<PlayerController>());
             Destroy(GetComponent<Motor>());
-            
-            Destroy(GetComponentInChildren<Camera>());
 
-            Destroy(this);
+            // Remote player does not need a Camera, or AudioListener, so we simply
+            // search for the Object with the Camera, and delete it entirely since
+            // it also has the AudioListener attached.
+            Destroy(GetComponentInChildren<Camera>().gameObject);
+
         }
+
+        // This componenet has now doen its job, so we can also Destroy it
+        Destroy(this);
     }
 }
