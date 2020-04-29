ab0e0563e09a7dd6d5706d715913dadf39dd8007
Damion Sheppard <damionlsheppard@gmail.com>
2020-04-29 10:49:44 +0100

    Added componenet to Player that will Destroy the Controller if the player is notLocal

---
+++ Assets/Scripts/Behaviour/Network/PhotonPlayerView.cs
@@ -0,0 +1,16 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+[RequireComponent(typeof(PhotonView))]
+[RequireComponent(typeof(PlayerController))]
+public class PhotonPlayerView : MonoBehaviourPun
+{
+    [SerializeField] PlayerController playerController;
+
+    private void Start()
+    {
+        if (!PhotonNetwork.IsConnected) return;
+
+        if (!photonView.IsMine) DestroyImmediate(playerController);
+    }
+}
