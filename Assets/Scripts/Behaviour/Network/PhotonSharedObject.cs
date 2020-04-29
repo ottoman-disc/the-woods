f609974973ef4640cffa66a1b78b69a9f037ac93
Damion Sheppard <damionlsheppard@gmail.com>
2020-04-29 15:58:06 +0100

    Experimenting with 'transfering ownership', so that a player intereacting physically with the tree can get local feeling behaviour

---
+++ Assets/Scripts/Behaviour/Network/PhotonSharedObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class PhotonSharedObject : MonoBehaviour, ITransferable
+{
+    public void Transfer()
+    {
+        PhotonView pv = this.GetComponent<PhotonView>();
+
+        pv.RequestOwnership();
+    }
+}
--- Assets/Scripts/Behaviour/Tree.cs
+++ Assets/Scripts/Behaviour/Tree.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.U2D;
 
-public class Tree : MonoBehaviour, IInteractable
+public class Tree : PhotonSharedObject, IInteractable
 {
     public void Interact()
     {
         GetComponent<SpriteShapeRenderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+
+        Transfer();
     }
 }
