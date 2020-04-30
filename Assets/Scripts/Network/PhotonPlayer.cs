be28a18e5973cd24cc997f5761557bbd1df324db
Damion Sheppard <damionlsheppard@gmail.com>
2020-04-30 11:45:22 +0100

    You can now clearly see who 'owns' an object by it's object name...

--- Assets/Scripts/Network/PhotonPlayer.cs
+++ Assets/Scripts/Network/PhotonPlayer.cs
@@ -9,8 +9,14 @@ namespace OttomanDisc
         {
             if (!PhotonNetwork.IsConnected) return;
 
-            if (!photonView.IsMine)
+            if (photonView.IsMine)
             {
+                this.name = "PLAYER: LOCAL";
+            }
+            else
+            {
+                this.name = "PLAYER: REMOTE";
+
                 // Remote player does not need a Controller or Motor. Movement driven
                 // by them locally, and synced to us.
                 Destroy(GetComponent<PlayerController>());
