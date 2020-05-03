b4824d1e4d163549855602bd72f11592dbe8b5ca
Scott Matthews <scottmatthews343@gmail.com>
2020-05-03 16:20:37 +0100

    Style experiment (#12)
    * experimenting with 2D objects in 3D space
    
    * wip
    
    * live demo
    
    * breaking change
    
    * finish

---
+++ Assets/_Main/Scripts/Behaviour/MotorXZ.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace OttomanDisc
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class MotorXZ : MonoBehaviour
+    {
+        private Rigidbody _rb;
+        private Vector3 _direction;
+        private bool moving = false;
+        private const int speed = 2;
+
+        private void Awake()
+        {
+            _rb = this.GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (moving) _rb.velocity = _direction * speed;
+        }
+
+        public void Move(Vector3 direction)
+        {
+            _rb.drag = 0f;
+            _direction = direction;
+            moving = true;
+        }
+
+        public void Stop()
+        {
+            _rb.drag = 50f;
+            moving = false;
+        }
+    }
+}
---
+++ Assets/_Main/Scripts/Behaviour/MotorXZController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace OttomanDisc
+{
+    [RequireComponent(typeof(MotorXZ))]
+    public class MotorXZController : MonoBehaviour
+    {
+        private PlayerInputActions inputActions;
+
+        private MotorXZ moter;
+
+        private void Awake()
+        {
+            inputActions = new PlayerInputActions();
+
+            moter = GetComponent<MotorXZ>();
+        }
+
+        private void OnEnable()
+        {
+            inputActions.Game.Enable();
+
+            inputActions.Game.Move.performed += OnMove;
+            inputActions.Game.Move.canceled += OnMoveStop;
+        }
+
+        private void OnMove(InputAction.CallbackContext context)
+        {
+            var direction2D = context.ReadValue<Vector2>();
+            var direction = new Vector3{
+                x = direction2D.x,
+                y = 0,
+                z = direction2D.y
+            };
+            Debug.LogFormat("Moving {0}", direction);
+            moter.Move(direction);
+        }
+
+        private void OnMoveStop(InputAction.CallbackContext context)
+        {
+            moter.Stop();
+        }
+
+        private void OnDisable()
+        {
+            inputActions.Game.Move.canceled -= OnMoveStop;
+            inputActions.Game.Move.performed -= OnMove;
+
+            inputActions.Game.Disable();
+        }
+    }
+}
