f7b32c41363b55729302c110279319a79dc054af
DamionSheppard <d.l.sheppard@live.co.uk>
2020-05-04 15:25:05 +0100

    Setting up new 3D scene with Photon Networking (#13)
    * Made the Build window smaller
    
    * Abstracting the 'Destroying' part of PhotonPlayer into a new 'Destroyer' class. This allows us to configure which Components and GameObjects are destroyed. This means we can (after a bit of setup) keep the old Player Prefab working.
    
    Parented the Camera to the Player.
    
    Made the Virtual Camera into a prefab itself.
    
    * Applied the new Destroyer settings to the old Player Prefab. Should continue to function.
    
    * Moved and renamed my Scene file (I recommend we both do this)
    
    * Froze appropriate axis on the player rigidbody
    
    Assigned renamed scene in build settings

---
+++ Assets/_Main/Scripts/General/Destroyer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace OttomanDisc
+{
+    public class Destroyer : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] gameObjectsToDestroy;
+        [SerializeField] private MonoBehaviour[] componentsToDestroy;
+ 
+        public void Activate()
+        {
+            foreach (GameObject go in gameObjectsToDestroy)
+                Destroy(go);
+
+            foreach (MonoBehaviour mb in componentsToDestroy)
+                Destroy(mb);
+        }
+    }
+}
