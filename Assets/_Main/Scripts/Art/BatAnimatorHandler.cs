f4f44c3f348e86b66d80f7129aba7d3d6aef1778
Damion Sheppard <damionlsheppard@gmail.com>
2020-05-11 15:48:51 +0100

    wip

---
+++ Assets/_Main/Scripts/Art/BatAnimatorHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace OttomanDisc.Art
+{
+    public class BatAnimatorHandler : AnimatorHandler
+    {
+        private static readonly int FlyingParameter = Animator.StringToHash("isFlying");
+
+        private IMotor motor;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            motor = GetComponentInParent<IMotor>();
+        }
+
+        private void Update()
+        {
+            if (motor.IsMoving) TakeOff();
+            else Settle();
+        }
+
+        public void Settle() => animator.SetBool(FlyingParameter, false);
+
+        public void TakeOff() => animator.SetBool(FlyingParameter, true);
+    }
+}
