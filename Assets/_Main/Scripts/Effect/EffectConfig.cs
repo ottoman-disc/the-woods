9ca875b17f3c165f67e6a07fea2f83451c58f5b1
Damion Sheppard <damionlsheppard@gmail.com>
2020-05-12 18:21:43 +0100

    BLOOD. Added an EffectConfig class for defining different effects to spawn. Added a simple pixel blood thing, plus a bigger one for DEATH

---
+++ Assets/_Main/Scripts/Effect/EffectConfig.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace OttomanDisc
+{
+    [CreateAssetMenu(fileName = "Effect", menuName = nameof(OttomanDisc) + "/" + nameof(EffectConfig))]
+    public class EffectConfig : ScriptableObject
+    {
+        [SerializeField] GameObject prefabToSpawn;
+
+        public void Spawn(Vector3 position, Quaternion rotation)
+        {
+            Instantiate(prefabToSpawn, position, rotation);
+        }
+
+        public void Spawn(Vector3 position)
+        {
+            Spawn(position, default);
+        }
+    }
+}
