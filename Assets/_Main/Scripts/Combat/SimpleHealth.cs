810d5cdc9957e00d420cb2e93e9f8098cc297c87
DamionSheppard <d.l.sheppard@live.co.uk>
2020-05-14 17:15:56 +0100

    Combat attack base and blood spatter effect test (#21)
    * Simple damage on space bar - using IDamage and IDamageable, with a Hit component via an OverlapSphere. We could drive this from an animation (maybe) later
    
    * BLOOD. Added an EffectConfig class for defining different effects to spawn. Added a simple pixel blood thing, plus a bigger one for DEATH
    
    * wip
    
    * Finished up scene
    
    * blood 'pixels' size closer to sprite pixels
    
    Co-authored-by: Damion Sheppard <damionlsheppard@gmail.com>

---
+++ Assets/_Main/Scripts/Combat/SimpleHealth.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace OttomanDisc
+{
+    public class SimpleHealth : MonoBehaviour, IDamageable
+    {
+        [SerializeField] int health = 10;
+        [SerializeField] EffectConfig damageEffect;
+        [SerializeField] EffectConfig deathEffect;
+
+        Transform _transform;
+
+        private void Awake()
+        {
+            _transform = this.transform;
+        }
+
+        public void DamageReceived(IDamage damageValue)
+        {
+            health -= damageValue.Damage;
+            damageEffect.Spawn(_transform.position);
+
+            if (health == 0) Die();
+        }
+
+        public void Die()
+        {
+            deathEffect.Spawn(_transform.position);
+
+            Destroy(this.gameObject);
+        }
+    }
+}
