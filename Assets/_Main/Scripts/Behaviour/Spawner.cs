a0bf0bd07daf7e9e1d260c94576c2c81bbee448c
Damion Sheppard <damionlsheppard@gmail.com>
2020-05-17 12:57:22 +0100

    Added Spawner to OttomanDisc namespace

--- Assets/_Main/Scripts/Behaviour/Spawner.cs
+++ Assets/_Main/Scripts/Behaviour/Spawner.cs
@@ -2,30 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+namespace OttomanDisc
 {
-    [SerializeField]
-    private float spawnDelay = 1f;
-
-    [SerializeField]
-    private int spawnCount = 5;
-
-    [SerializeField]
-    private Bat enemyPrefab;
-
-    IEnumerator Start()
+    public class Spawner : MonoBehaviour
     {
-        for (var i = 0; i < spawnCount; i++)
+        [SerializeField]
+        private float spawnDelay = 1f;
+
+        [SerializeField]
+        private int spawnCount = 5;
+
+        [SerializeField]
+        private Bat enemyPrefab;
+
+        IEnumerator Start()
         {
-            yield return new WaitForSeconds(spawnDelay);
-            Spawn();
+            for (var i = 0; i < spawnCount; i++)
+            {
+                yield return new WaitForSeconds(spawnDelay);
+                Spawn();
+            }
         }
-    }
 
-    private void Spawn()
-    {
-        var spawnPos = transform.position + new Vector3(Random.Range(0, 5), 0, Random.Range(0, 5));
-        Instantiate(enemyPrefab, spawnPos, transform.rotation);
-        Debug.Log("Spawned new ememy");
+        private void Spawn()
+        {
+            var spawnPos = transform.position + new Vector3(Random.Range(0, 5), 0, Random.Range(0, 5));
+            Instantiate(enemyPrefab, spawnPos, transform.rotation);
+            Debug.Log("Spawned new ememy");
+        }
     }
 }
